3f51b2fd3f6706653be140aea2754d5c76cbf8c3
Gosia Kabaczynska <mkabaczynska@wp.pl>
2020-06-07 17:30:23 +0100

    scripts added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
-"use strict";
-document.getElementById("test-button").addEventListener("click", function () {
+/*document.getElementById("test-button").addEventListener("click", function () {
   const links = document.querySelectorAll(".titles a");
   console.log("links:", links);
-});
+});*/
+const titleClickHandler = function () {
+  console.log("Link was clicked!");
+
+  /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll(".titles a.active");
+
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove("active");
+  }
+
+  /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll(".content a.active");
+
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove("active");
+  }
+
+  /* add class 'active' to the clicked link */
+
+  /* remove class 'active' from all articles */
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+};
+
+const links = document.querySelectorAll(".titles a");
+
+for (let link of links) {
+  link.addEventListener("click", titleClickHandler);
+  console.log("Link was clicked!");
+}
